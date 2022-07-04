dab8bf5d126f773b70490ccbc49857d057e15b0a
anahips <102506256+anahips@users.noreply.github.com>
2022-07-04 22:38:40 +0100

    fixed bugs

--- src/Definitions.js
+++ src/Definitions.js
@@ -4,8 +4,14 @@ export default function Definitions(props) {
   return (
     <div className="definitions">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>Definition: {props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <p>{definition.example}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
