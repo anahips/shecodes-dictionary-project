f0566d68f05c360bfeefb4d4624e53f67af97086
anahips <102506256+anahips@users.noreply.github.com>
2022-07-04 22:55:35 +0100

    removed mapping for definitions
    replaced it with something simpler

--- src/Definitions.js
+++ src/Definitions.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 export default function Definitions(props) {
+  console.log(props.meaning);
   return (
     <div className="definitions">
       <h4>{props.meaning.partOfSpeech}</h4>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>{definition.definition}</p>
-            <p>{definition.example}</p>
-          </div>
-        );
-      })}
+      <p>{props.meaning.definitions[0].definition}</p>
+      <p>
+        <em>{props.meaning.definitions[0].example}</em>
+      </p>
     </div>
   );
 }
