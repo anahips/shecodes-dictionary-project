682505747abd108977383bf5dd1edd84978e37fa
anahips <102506256+anahips@users.noreply.github.com>
2022-07-07 21:25:00 +0100

    fixed bug
    added rel="noreferrer"

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetics(props) {
   return (
     <div className="phonetics">
-      <a href={props.phonetics.audio} target="_blank">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         Hear me
       </a>
       <p>{props.phonetics.text}</p>
