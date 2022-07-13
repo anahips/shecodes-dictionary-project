fde3265b46567dd471e502dac974f91ae5b28ee3
anahips <102506256+anahips@users.noreply.github.com>
2022-07-13 22:13:39 +0100

    added footer and background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import SearchForm from "./SearchForm";
+import background from "./balloonbg.jpg";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <h1>Dictionary</h1>
-        </header>
-        <SearchForm defaultWord="smile" />
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="App">
+        <div className="container">
+          <header className="App-header">
+            <h1>Dictionary</h1>
+          </header>
+          <SearchForm defaultWord="smile" />
+          <footer>
+            This project was coded by{" "}
+            <a href="https://www.linkedin.com/in/ana-hipolito/">Ana Hipolito</a>{" "}
+            and is{" "}
+            <a href="https://github.com/anahips/shecodes-dictionary-project">
+              open sourced
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
